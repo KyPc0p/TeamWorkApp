fd79459d9a545a8da6b043d275a1e4eff0672a6f
Artem <artem.kh4rchenko@ya.ru>
2022-04-01 17:13:10 +0700

    15.2

---
+++ TeamWorkApp/ViewController 00.18.34.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamWorkApp
+//
+//  Created by Артём Харченко on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Тут что то просиходит нет не правда ничего
+    }
+
+
+}
+
