06c6fb177f369504ac38bab599f705f8e2816422
Nazar <nazar383875@gmail.com>
2022-04-03 13:13:14 +0300

    gotovo

---
+++ TeamWorkApp/NewsTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  NewsTableViewCell.swift
+//  TeamWorkApp
+//
+//  Created by Назар Ткаченко on 03.04.2022.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    override func layoutSubviews() {
+     super.layoutSubviews()
+     self.avatarImage.layer.cornerRadius = self.avatarImage.frame.width / 2
+     self.avatarImage.clipsToBounds = true
+    }
+}
