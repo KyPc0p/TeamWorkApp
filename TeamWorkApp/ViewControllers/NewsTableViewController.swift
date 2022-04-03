667dd4a65410408fa4c4a9d067607ef856d812ec
Nazar <nazar383875@gmail.com>
2022-04-03 13:55:29 +0300

    new files

---
+++ TeamWorkApp/ViewControllers/NewsTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NewsTableViewController.swift
+//  TeamWorkApp
+//
+//  Created by Назар Ткаченко on 03.04.2022.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    var user = User.getUserData()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = view.frame.height / 2
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return user.listOfFriends.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let friend = user.listOfFriends[indexPath.row]
+        let photo = friend.profilePhotos[0]
+        let news = friend.profileAvatar
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "newsTableViewSell"
+        ) as? NewsTableViewCell else { return UITableViewCell() }
+        cell.nameLabel.text = news
+        cell.avatarImage.image = UIImage(named: news)
+        cell.photoImage.image = UIImage(named: photo)
+        cell.timeLabel.text = "Added a new photo \(Int.random(in: 1...24)) hours ago "
+        return cell
+    }
+}
